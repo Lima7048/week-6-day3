eeb07ee8a9644b5c12e760c3c57071c14a9566d3
salima <brycesalima@gmail.com>
2024-02-14 15:22:46 +0000

    timer pure functions

---
+++ UseEffect-and-timers/src/components/TimerDirty.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import "./App.jsx";
+
+export default function TimerDirty() {
+  console.log("TimerDirty component render");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("TimerDirty component useEffect callback");
+    setInterval(() => {
+      setCount((currentCount) => (currentCount = 1));
+    }, 1000);
+  }, []);
+
+  return <div>Count:{count}</div>;
+}
